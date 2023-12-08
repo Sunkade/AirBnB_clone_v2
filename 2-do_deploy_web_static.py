1c8821370647934df4b702901a88e457d8253e12
Sunkade <sunkadefasola@gmail.com>
2023-12-08 01:29:48 +0100

    3-deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script for deploying an archive to web servers using do_deploy function.
-
-Usage: fab -f 2-do_deploy_web_static.py do_deploy:archive_path=<path to archive> -i <SSH private key> -u <username>
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import env, put, run
+from fabric.api import put, run, env
 from os.path import exists
-
-env.hosts = ['100.27.14.93', '54.162.87.128']
-env.user = 'ubuntu'
+env.hosts = ["100.27.14.93", "54.162.87.128"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-    if not exists(archive_path):
-        print(f"Archive not found: {archive_path}")
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        archive_name = archive_path.split("/")[-1]
-        base_name = archive_name.split(".")[0]
-        remote_path = "/data/web_static/releases/"
-
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run(f'mkdir -p {remote_path}{base_name}/')
-        run(f'tar -xzf /tmp/{archive_name} -C {remote_path}{base_name}/')
-        run(f'rm /tmp/{archive_name}')
-        run(f'mv {remote_path}{base_name}/web_static/* {remote_path}{base_name}/')
-        run(f'rm -rf {remote_path}{base_name}/web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {remote_path}{base_name}/ /data/web_static/current')
-        print("New version deployed!")
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print(f"Error deploying archive: {e}")
+    except:
         return False
-
